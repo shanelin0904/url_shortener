a62ee6ca33b5f887b4d2b5eb2d0e29c6d7ef872b
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-19 00:47:56 +0800

    Server starting up

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const exphbs = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 //樣板引擎
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// 這是設定啥
+app.use(bodyParser.urlencoded({ extended: true }))
+// 啟動伺服器
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
+})
