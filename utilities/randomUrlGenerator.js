1168a6d5b85515a2a1924b50524f1826b3724207
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-19 03:22:12 +0800

    add 「random code generator」 js file

---
+++ utilities/randomUrlGenerator.js
@@ -0,0 +1,12 @@
+function urlRandomGenrator(){
+  let text = ""
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+
+  return text
+}
+
+console.log(urlRandomGenrator())
+module.exports = urlRandomGenrator
